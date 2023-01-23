47c536e19490396510b167475ae4b5d44d444d68
skullzado <pafcalzado@gmail.com>
2023-01-23 13:02:34 +0800

    Adds Player initialization to the game

--- GameGitDemo/Game.cs
+++ GameGitDemo/Game.cs
@@ -6,6 +6,14 @@ internal class Game
     public void Run()
     {
         WriteLine("=== Welcome to the Game ===");
+
+        Write("What is your name? ");
+        string name = ReadLine();
+   
+        Player currentPlayer = new(name);
+
+        WriteLine($"Let's get started, {currentPlayer.Name}!");
+
         WriteLine("\nPress any key to exit...");
         ReadKey(true);
     }
